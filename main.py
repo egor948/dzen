4771531793dc1fba3aac8a8ccccec046cbd330db
egor948 <40360711+egor948@users.noreply.github.com>
2025-09-27 12:05:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import asyncio
 # Telethon
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 client = TelegramClient("session", API_ID, API_HASH)
 
